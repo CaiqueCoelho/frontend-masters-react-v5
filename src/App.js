ca3e5f80ba8f149a7b39b45146e91737f2fa2413
caiquecoelhoqa <caique.coelho@quintoandar.com.br>
2020-10-11 00:09:19 -0300

    generic useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Seni",
-      animal: "dog",
-      breed: "Pastor Alemão",
-    }),
-    React.createElement(Pet, { name: "Kuduro", animal: "Cat", breed: "Mixed" }),
-    React.createElement(Pet, { name: "Nemo", animal: "fish", breed: "Beta" }),
-  ]);
+  return (
+    <React.StrictMode>
+      <div>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));

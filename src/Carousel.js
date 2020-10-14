d2fb8b1544e3f08cc6331135e64014a3c06dccd6
caiquecoelhoqa <caique.coelho@quintoandar.com.br>
2020-10-14 01:31:31 -0300

    details route and carousel

---
+++ src/Carousel.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: ["http://placecorgi.com/600/600"],
+    active: 0,
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    console.log(photos);
+
+    return { photos };
+  }
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+
+    console.log(photos);
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
